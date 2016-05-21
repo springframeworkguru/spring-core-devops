83699f30ccd037af4d52c54796fdf59b2b382148
John Thompson <john@springframework.guru>
2016-05-21 14:49:26 -0400

    Testing Active Profiles

--- src/test/java/guru/springframework/test/ds/DevDataSource.java
+++ src/test/java/guru/springframework/test/ds/DevDataSource.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
  * Created by jt on 5/21/16.
  */
 @Component
-@Profile("dev")
+@Profile({"dev", "default"})
 public class DevDataSource implements FakeDataSource {
     @Override
     public String getConnectionInfo() {
---
+++ src/test/java/guru/springframework/test/dstest/DataSourceTest.java
@@ -0,0 +1,34 @@
+package guru.springframework.test.dstest;
+
+/**
+ * Created by jt on 5/21/16.
+ */
+
+import guru.springframework.test.config.DataSourceConfig;
+import guru.springframework.test.ds.FakeDataSource;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(DataSourceConfig.class)
+@ActiveProfiles("guru")
+public class DataSourceTest {
+
+    private FakeDataSource fakeDataSource;
+
+    @Autowired
+    public void setFakeDataSource(FakeDataSource fakeDataSource) {
+        this.fakeDataSource = fakeDataSource;
+    }
+
+    @Test
+    public void TestDataSource() throws Exception {
+
+        System.out.println(fakeDataSource.getConnectionInfo());
+
+    }
+}
