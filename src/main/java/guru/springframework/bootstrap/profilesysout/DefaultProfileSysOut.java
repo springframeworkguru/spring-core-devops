e77338a3f15131db5908191e5b37c0c094992452
John Thompson <john@springframework.guru>
2016-05-21 15:54:12 -0400

    Profiles at Runtime

--- src/main/java/guru/springframework/bootstrap/profilesysout/DefaultProfileSysOut.java
+++ src/main/java/guru/springframework/bootstrap/profilesysout/DefaultProfileSysOut.java
@@ -1,5 +1,7 @@
 package guru.springframework.bootstrap.profilesysout;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -10,10 +12,12 @@ import org.springframework.stereotype.Component;
 @Profile("default")
 public class DefaultProfileSysOut {
 
-    public DefaultProfileSysOut() {
+    @Autowired
+    public DefaultProfileSysOut(@Value("${guru.springframework.profile.message}") String msg) {
         System.out.println("##################################");
         System.out.println("##################################");
         System.out.println("##            DEFAULT           ##");
+        System.out.println(msg);
         System.out.println("##################################");
         System.out.println("##################################");
     }
