63240cb860ceb7226af54c1f3932c9a65fcf57bd
John Thompson <john@springframework.guru>
2016-10-09 15:39:29 -0400

    testing CI build

--- src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
+++ src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
@@ -9,6 +9,6 @@ public class SpringCoreDevOpsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCoreDevOpsApplication.class, args);
 
-		//this is a whitespace change
+		//this is a whitespace change test
 	}
 }
